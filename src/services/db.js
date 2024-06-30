56d8b501acc5f93504d43bf1d8584320bc87c886
Edgern2 <cxj69.23@ichat.sp.edu.sg>
2024-06-30 19:04:33 +0800

    bed lab 4

--- src/services/db.js
+++ src/services/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const setting = {
     connectionLimit : 10, //set limit to 10 connection
-    host     : 'WRITE YOUR HOST HERE',
-    user     : 'WRITE YOUR USER HERE',
-    password : 'WRITE YOUR PASSWORD HERE',
-    database : 'WRITE YOUR DATABASE HERE',
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Giveme4.0GPA',
+    database : 'pokemon',
     multipleStatements: true, //allow multiple sql statements
     dateStrings: true //return date as string instead of Date object
 }
