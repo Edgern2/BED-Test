bb61920778ef9362065973010bb5d4c265e80b0a
Edgern2 <cxj69.23@ichat.sp.edu.sg>
2024-06-30 19:01:03 +0800

    BED-testing

---
+++ lab1/app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+fs.readFile("greeting.txt" , (err , data) =>{
+
+    let objLog;
+
+    if (err) objLog = {message:err.message};
+    else objLog = {message:data.toString()};
+
+    let jsonData = JSON.stringify(objLog, null, "\t");
+    console.log(jsonData);
+});
